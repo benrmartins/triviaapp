8ccd1b077c7b837c0f9b47bfa12a2c19ae9ebff3
benrmartins <benrmartins@gmail.com>
2022-06-02 20:17:05 -0400

    trivia app

---
+++ public/game.js
@@ -0,0 +1,188 @@
+let questionNumber = 0;
+let score = 0;
+let totalScore = 0;
+let totalAnswered = 0;
+let rightAnswer = ""
+let id = document.getElementById("id").innerText
+id = parseInt(id, 10)
+
+window.onload = async () => {
+    await getScore().then(allScores => {
+        const lbDiv = document.getElementById("leaderboardData")
+        const scoreTotal = document.getElementById("totals")
+        const totalElm = document.createElement("p")
+
+        for(let i = allScores.length - 1 ; i >= allScores.length - 6; i--) {
+            if(allScores[i].login_id == id) {
+                const scoreElm = document.createElement("p")
+                const scoreData = allScores[i]
+                scoreElm.innerText = `Username: ${scoreData.Name} | Difficulty: ${scoreData.difficulty} | Score: ${scoreData.total_correct} / ${scoreData.total_correct + scoreData.total_wrong}`
+                lbDiv.appendChild(scoreElm)
+            }
+        }
+
+        for(let i = 0; i < allScores.length; i++) {
+            if(allScores[i].login_id == id) {
+                const scoreData = allScores[i]
+                totalScore += scoreData.total_correct
+                totalAnswered += scoreData.total_correct + scoreData.total_wrong
+            }
+        }
+
+        if(totalScore != 0) {
+            totalElm.innerText = `Total Correct/Total Answered: ${totalScore} / ${totalAnswered} or ${Math.round(totalScore/totalAnswered * 100)}%`
+            scoreTotal.appendChild(totalElm)
+        }
+        
+        
+    })
+}
+
+
+const removeClass = () => {
+   document.getElementById('startBox').style.display = "block"
+   retrieve()
+   questionNumber = 0
+   score = 0
+}
+
+async function getData() {
+    let response = await fetch('https://opentdb.com/api.php?amount=50&type=multiple')
+    response = await response.json()
+    return response
+}
+
+async function getScore() {
+    let response = await fetch('http://localhost:8000/score')
+    response = await response.json()
+    return response
+}
+
+async function postScore(newScore) {
+    let response = await fetch('http://localhost:8000/score', { method:'post', body: JSON.stringify(newScore), headers: {'Content-Type': 'application/json'}} )
+    response = await response.json()
+    return response
+}
+
+const retrieve = () => {
+    getData().then((value) => {
+        trivia(value)
+    })
+}
+
+document.getElementById("find").addEventListener("click", removeClass)
+
+const trivia = (data) => {
+    console.log(data)
+    const totalNum = document.getElementById("num").value
+    if (questionNumber <= totalNum) {
+        for(let i = 0; i < data.results.length; i++) {
+            if(data.results[i].difficulty === document.getElementById('difficult').value) {
+                rightAnswer = data.results[i].correct_answer
+                let incorrectAnswer = data.results[i].incorrect_answers
+                let list = incorrectAnswer
+                list.splice(Math.floor(Math.random() * (incorrectAnswer.length + 1)), 0, rightAnswer)
+                questions = data.results[i].question
+
+                document.getElementById("question").innerHTML = `Question: ${questions}`
+
+                let answer1 = list[0]
+                let answer2 = list[1]
+                let answer3 = list[2]
+                let answer4 = list[3]
+
+                document.querySelector("#answer1").innerHTML = answer1
+                document.querySelector("#answer2").innerHTML = answer2
+                document.querySelector("#answer3").innerHTML = answer3
+                document.querySelector("#answer4").innerHTML = answer4
+
+                document.getElementById("score").innerHTML = `Score: ${score}/${questionNumber}`
+                document.getElementById("difficulty").innerHTML = "Difficulty: " + data.results[i].difficulty
+
+
+                reset()
+
+                document.getElementById('answer1').disabled = false;
+                document.getElementById('answer2').disabled = false;
+                document.getElementById('answer3').disabled = false;
+                document.getElementById('answer4').disabled = false;
+                break;
+            } 
+        }
+        questionNumber++
+    } else {
+        questionNumber = questionNumber - 1
+        document.getElementById('usermessage').innerHTML = `Game over. You have answered ${questionNumber} questions. You got ${Math.floor(score/(questionNumber) * 100)}% right!`
+        postScore({loginID: id, difficulty: document.getElementById('difficult').value , correctQuestions: score, incorrectQuestion: questionNumber - score})
+    }
+}
+
+
+const checkAnswer1 = () => {
+    if(rightAnswer === document.getElementById("answer1").textContent) {
+        score++
+        document.getElementById("rightwrong").innerHTML = "You selected the correct answer"
+    } else {
+        document.getElementById("rightwrong").innerHTML = "You selected the incorrect answer. The correct answer is: " + rightAnswer
+    }
+
+    document.getElementById('answer1').disabled = true;
+    document.getElementById('answer2').disabled = true;
+    document.getElementById('answer3').disabled = true;
+    document.getElementById('answer4').disabled = true;
+}
+
+const checkAnswer2 = () => {
+    if(rightAnswer === document.getElementById("answer2").textContent) {
+        score++
+        document.getElementById("rightwrong").innerHTML = "You selected the correct answer"
+    } else {
+        document.getElementById("rightwrong").innerHTML = "You selected the incorrect answer. The correct answer is: " + rightAnswer
+    }
+
+    document.getElementById('answer1').disabled = true;
+    document.getElementById('answer2').disabled = true;
+    document.getElementById('answer3').disabled = true;
+    document.getElementById('answer4').disabled = true;
+}
+
+const checkAnswer3 = () => {
+    if(rightAnswer === document.getElementById("answer3").textContent) {
+        score++
+        document.getElementById("rightwrong").innerHTML = "You selected the correct answer"
+    } else {
+        document.getElementById("rightwrong").innerHTML = "You selected the incorrect answer. The correct answer is: " + rightAnswer
+    }
+
+    document.getElementById('answer1').disabled = true;
+    document.getElementById('answer2').disabled = true;
+    document.getElementById('answer3').disabled = true;
+    document.getElementById('answer4').disabled = true;
+}
+
+const checkAnswer4 = () => {
+    if(rightAnswer === document.getElementById("answer4").textContent) {
+        score++
+        document.getElementById("rightwrong").innerHTML = "You selected the correct answer"
+    } else {
+        document.getElementById("rightwrong").innerHTML = "You selected the incorrect answer. The correct answer is: " + rightAnswer
+    }
+    document.getElementById('answer1').disabled = true;
+    document.getElementById('answer2').disabled = true;
+    document.getElementById('answer3').disabled = true;
+    document.getElementById('answer4').disabled = true;
+}
+
+const restartGame = () => {
+    document.getElementById('usermessage').innerHTML = ''
+    retrieve()
+    questionNumber = 0
+    score = 0
+    document.getElementById('startBox').style.display = "none"
+    location.reload()
+
+}
+
+const reset = () => {
+    document.getElementById("rightwrong").innerHTML = "" 
+}
---
+++ trivia.js
@@ -0,0 +1,125 @@
+const express = require('express')
+const app = express()
+path = require('path')
+global.globalString = ""
+global.globalId = 0
+
+
+const fs = require('fs')
+const folderPath = '/Users/benjaminmartins/Documents/careerDevs/cohort18/202205/triviaapp/'
+
+app.use(express.static('public'))
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    port: 8889,
+    password: 'root',
+    database: 'TriviaApp',
+})
+
+
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+    
+})
+
+app.post('/login', (req, res) => {
+    const name = req.body.name
+    const password = req.body.password
+
+   db.query("SELECT * FROM Login", function (err, result, fields) {
+       if (err) throw err
+       let bol = true
+       for(let i = 0; i < result.length; i++) {
+           if(result[i].Name == name && result[i].Password == password) {
+               globalString = result[i].Name
+               globalId = result[i].idLogin
+               res.redirect('/')
+               bol = false
+               break
+           }
+       }
+       if(bol) {
+            console.log("not correct information")
+            res.status(401).send("Invaild credentials")
+       }
+})
+
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
+
+app.post('/register', (req, res) => {
+    let name = req.body.name
+    let password = req.body.password
+
+    db.query("SELECT * FROM Login", function (err, result, fields) {
+        let bool = true
+        if (err) throw err
+        for(let i = 0; i < result.length; i++) {
+            if(result[i].Name === name || result[i].Password === password) {
+                bool = false
+                break
+
+            }
+        }
+        if(bool) {
+            db.query('INSERT INTO Login (Name, Password) VALUES (?, ?)', [name, password], (err, result) => {
+                if(err) {
+                    console.log(err)
+                }
+                res.redirect('/login');
+            })
+        } else {
+            res.status(401).send("Username or password is already taken")
+
+        }
+    })
+    
+})
+
+app.post('/score', (req, res) => {
+    const {loginID, difficulty, correctQuestions, incorrectQuestion} = req.body
+    try {
+        db.query('INSERT INTO Score (login_id, difficulty, total_correct, total_wrong) VALUES (?, ?, ?, ?)', [loginID, difficulty, correctQuestions, incorrectQuestion], (err, result) => {
+            if(err) {
+                return res.status(500).send(err.message)
+            }
+            res.redirect('/');
+        })
+
+    } catch(err) {
+        res.status(500).send(err.message)
+    }
+   
+})
+
+app.get('/score', (req, res) => {
+    db.query("SELECT * FROM Score JOIN Login ON Score.login_id=Login.idlogin", function (err, result, fields) {
+        if(err) return res.status(500).send(err.message)
+        res.json(result)
+        
+    })
+
+})
+
+
+app.get('/', (req, res) => {
+    res.render('start.ejs', { name: globalString, id: globalId })
+})
+
+app.post('/', (req, res) => {
+})
+
+app.listen(8000, () => {
+    console.log('server is now running');
+})
